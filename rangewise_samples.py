f60a641d2c7f90be8b3dcaf71210aad724bd695f
Gunaseelan Rajamanickam (MS/ESV31-CC) <ugn1cob@bosch.com>
2022-03-15 21:27:14 +0530

    Chnages related to BDD

--- rangewise_samples.py
+++ rangewise_samples.py
@@ -1,3 +1,36 @@
 def infers_readings(readings):
-  readings.sort()
-  return f'{readings[0]}-{readings[-1]}, {len(readings)}'
+  validReadings = isSequenceOk(readings)
+  if validReadings == True:
+    readings.sort()
+    sortedReadings = []
+    sortedReadings.append(sortReadings(readings))
+    groups = detectRange(readings)
+    for list in groups:
+      formattedString = convertCSVFormat(list)
+      printOnConsole(formattedString)
+    return formattedString
+  return False
+
+def isSequenceOk(readings):
+  if len(readings) <= 0:
+    return False
+  return True
+
+def sortReadings(readings):
+  return readings.sort()
+
+def detectRange(readings):
+  diff = [j-i for i, j in zip(readings[:-1], readings[1:])]
+  diff.insert(0, 0)
+
+  ind = [i for i,v in enumerate(diff) if v >= 2]
+  ind.insert(0, 0)
+
+  groups = [readings[i:j] for i,j in zip(ind, ind[1:]+[None])]
+  return groups
+
+def convertCSVFormat(list):
+  return f'{list[0]}-{list[-1]}, {len(list)}'
+
+def printOnConsole(string):
+  print(string)
--- rangewise_samples.test.py
+++ rangewise_samples.test.py
@@ -1,9 +1,29 @@
 import unittest
 import rangewise_samples
+import io
+import sys
 
 class RangewiseTest(unittest.TestCase):
   def test_infers_readings_as_per_range(self):
     self.assertTrue(rangewise_samples.infers_readings([4,5]) == "4-5, 2")
+    self.assertTrue(rangewise_samples.infers_readings([3, 3, 5, 4, 10, 11, 12]) == "10-12, 3")
+    self.assertTrue(rangewise_samples.infers_readings([3]) == "3-3, 1")
+  
+  def test_isSequenceOk(self):
+    self.assertTrue(rangewise_samples.isSequenceOk([]) == False)
+
+  def test_detectRange(self):
+    self.assertTrue(rangewise_samples.detectRange([3, 3, 4, 5, 10, 11, 12]) == [[3,3,4,5],[10,11,12]])
+  
+  def test_convertCSVFormat(self):
+    self.assertTrue(rangewise_samples.convertCSVFormat([3,3,4,5]) == "3-5, 4")
+
+  def test_printOnConsole(self):
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    rangewise_samples.printOnConsole('All is fine!')
+    sys.stdout = sys.__stdout__
+    print ('Captured', capturedOutput.getvalue())
 
 if __name__ == '__main__': # pragma: no cover
   unittest.main()
